8415374f2ba7d942162ffc7b57f54d79581c7dcb
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2021-09-15 00:16:30 +0700

    :hammer: :rotating_light: Add eslint / prettier & husky

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,61 +1,60 @@
 import { useState } from 'react'
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import MarkdownEditor from "@monaco-editor/react";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math';
-import remarkFootnote from 'remark-footnotes';
-import rehypeKatex from 'rehype-katex';
-import CodeBlock from './CodeBlock';
+import MarkdownEditor from '@monaco-editor/react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remarkFootnote from 'remark-footnotes'
+import rehypeKatex from 'rehype-katex'
+import CodeBlock from './CodeBlock'
 
-import 'katex/dist/katex.min.css';
+import 'katex/dist/katex.min.css'
 
 const editorPlaceholder = `# Welcome!\n\n
 This is simplest markdown editor. You can write anything in this!\n\n
-**Note:** This support [Commonmark](https://commonmark.org/help/) and [GFM (Github Flavor Markdown)](https://github.github.com/gfm/)`;
+**Note:** This support [Commonmark](https://commonmark.org/help/) and [GFM (Github Flavor Markdown)](https://github.github.com/gfm/)`
 
 const Editor: React.FC = () => {
-  const [codeValue, setCodeValue] = useState<string>(editorPlaceholder);
+  const [codeValue, setCodeValue] = useState<string>(editorPlaceholder)
 
-  const handleEditorChange = (value: string | undefined, event: monaco.editor.IModelContentChangedEvent): void => {
+  const handleEditorChange = (value: string | undefined): void => {
     if (value !== undefined) {
-      setCodeValue(value);
+      setCodeValue(value)
     }
   }
 
   return (
     <>
       <div className="w-1/2 h-full">
-          <MarkdownEditor
-            defaultLanguage="markdown"
-            value={codeValue}
-            onChange={handleEditorChange}
-            options={{
-                minimap: {
-                enabled: false
-              },
-            }}
-          />
+        <MarkdownEditor
+          defaultLanguage="markdown"
+          value={codeValue}
+          onChange={handleEditorChange}
+          options={{
+            minimap: {
+              enabled: false,
+            },
+          }}
+        />
       </div>
       <div className="w-1/2 h-full overflow-y-auto">
         <ReactMarkdown
           skipHtml
           className="prose px-4 py-2"
-          rehypePlugins={[
-            rehypeKatex,
-          ]}
+          rehypePlugins={[rehypeKatex]}
           remarkPlugins={[
             remarkGfm,
             remarkMath,
-            [remarkFootnote, {inlineNotes: true}]
+            [remarkFootnote, { inlineNotes: true }],
           ]}
           components={{
-            code: CodeBlock
+            code: CodeBlock,
           }}
-        >{codeValue}</ReactMarkdown>
+        >
+          {codeValue}
+        </ReactMarkdown>
       </div>
     </>
-  );
+  )
 }
 
-export default Editor;
+export default Editor
---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '**/*.(ts|tsx)': (filenames) => [
+    'yarn tsc --noEmit',
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
+}
